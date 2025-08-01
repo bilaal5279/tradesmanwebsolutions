8578305a74a8e3eea33692dc74ba7ce1a69ff3bc
bilaal5279 <bilaal5279@gmail.com>
2025-08-01 16:13:07 +0100

    fixed posthog (again)

--- src/components/PostHogProvider.jsx
+++ src/components/PostHogProvider.jsx
@@ -1,35 +1,45 @@
-"use client";
+'use client'
 
-import { useEffect, Suspense } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import posthog from 'posthog-js';
+import { usePathname, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
+import { usePostHog } from 'posthog-js/react'
+
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+
+export default function PostHogProvider({ children }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+    })
+  }, [])
+
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  )
+}
 
 function PostHogPageView() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const posthog = usePostHog()
 
   useEffect(() => {
-    if (pathname) {
-      let url = window.origin + pathname;
+    if (pathname && posthog) {
+      let url = window.origin + pathname
       if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`;
+        url = url + `?${searchParams.toString()}`
       }
       posthog.capture('$pageview', {
         $current_url: url,
-      });
+      })
     }
-  }, [pathname, searchParams]);
+  }, [pathname, searchParams, posthog])
 
-  return null;
-}
-
-export default function PostHogProvider({ children }) {
-  return (
-    <>
-      <Suspense fallback={null}>
-        <PostHogPageView />
-      </Suspense>
-      {children}
-    </>
-  );
+  return null
 }
